759af5f2e3c13afab64f47de1e91cc9f3816a9fa
Karolina Meira <karolina.meira@hotmail.com>
2023-03-16 21:15:37 -0300

    aula03

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const path = require('path');
 
 const expressLayouts = require('express-ejs-layouts');
@@ -7,6 +6,7 @@ const expressLayouts = require('express-ejs-layouts');
 const { connect } = require('./models');
 
 const pokemonsRouter = require('./routes/pokemons');
+const batalhaRouter = require('./routes/batalha');
 
 const app = express();
 
@@ -15,9 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+// configurando arquivos estáticos 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // declarando rotas
 app.use('/pokemons',pokemonsRouter);
-
+app.use('/batalha', batalhaRouter);
 
 const porta = 3000;
 app.listen(porta, () => {
